dc275b015a08ab83bda56720b6a0b6b91381daf7
minju-kim98 <ducco705@snu.ac.kr>
2022-08-16 14:11:41 +0900

    added prototype

---
+++ assignment/comma_to_table.sql
@@ -0,0 +1,11 @@
+/*
+    PROCEDURE "COMMA_TO_TABLE" (
+		list				IN		VARCHAR2,
+		tablen				OUT		BINARY_INTEGER,
+		tab				    OUT		UNCL_ARRAY);
+	
+    PROCEDURE "COMMA_TO_TABLE" (
+		list				IN		VARCHAR2,
+		tablen				OUT		BINARY_INTEGER,
+		tab					OUT		LNAME_ARRAY);
+*/
---
+++ assignment/table_to_comma.sql
@@ -0,0 +1,11 @@
+/*
+	PROCEDURE "TABLE_TO_COMMA" (
+		tab				    IN		UNCL_ARRAY,
+		tablen				OUT		BINARY_INTEGER,
+		list				OUT		VARCHAR2);
+
+	PROCEDURE "TABLE_TO_COMMA" (
+		tab				    IN		LNAME_ARRAY,
+		tablen				OUT		BINARY_INTEGER,
+		list				OUT		VARCHAR2);
+*/
