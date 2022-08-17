aa3a238db9bd7e3038bd8d123415a262ce249179
mj3meal <59169458+minju-kim98@users.noreply.github.com>
2022-08-17 14:43:32 +0900

    Update table_to_comma.sql
    Added body of table_to_comma

--- assignment/table_to_comma.sql
+++ assignment/table_to_comma.sql
@@ -9,3 +9,34 @@
 		tablen				OUT		BINARY_INTEGER,
 		list				OUT		VARCHAR2);
 */
+
+create or replace procedure TABLE_TO_COMMA(
+  tab in DBMS_UTILITY.UNCL_ARRAY,
+  tablen out BINARY_INTEGER,
+  list out VARCHAR2
+)
+IS
+begin
+  tablen := tab.count;
+  for i in 1..tablen loop
+	list := list || tab(i) || ',';
+  end loop;
+  list := rtrim(list, ',');
+end;
+/
+
+
+create or replace procedure TABLE_TO_COMMA(
+  tab in DBMS_UTILITY.LNAME_ARRAY,
+  tablen out BINARY_INTEGER,
+  list out VARCHAR2
+)
+IS
+begin
+  tablen := tab.count;
+  for i in 1..tablen loop
+	list := list || tab(i) || ',';
+  end loop;
+  list := rtrim(list, ',');
+end;
+/
