4cc14933519fec6ba501452d96c86c22995290dc
minju-kim98 <ducco705@snu.ac.kr>
2022-08-17 14:41:19 +0900

    Added boby of table_to_comma

--- assignment/table_to_comma.sql
+++ assignment/table_to_comma.sql
@@ -9,3 +9,18 @@
 		tablen				OUT		BINARY_INTEGER,
 		list				OUT		VARCHAR2);
 */
+
+create or replace procedure TABLE_TO_COMMA(
+  tab in DBMS_UTILITY.UNCL_ARRAY,
+  tablen out BINARY_INTEGER,
+  list out VARCHAR2
+)
+IS
+begin
+  tablen := tab.count;
+  for j in 1..tablen loop
+	list := list || tab(j) || ',';
+  end loop;
+  list := rtrim(list, ',');
+end;
+/
