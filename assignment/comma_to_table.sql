1de92f1feaae69f8f92dce702557087fd7646b5d
eomhs <jesikajunior@hanmail.net>
2022-08-17 14:08:34 +0900

    Update comma_to_table

--- assignment/comma_to_table.sql
+++ assignment/comma_to_table.sql
@@ -26,3 +26,20 @@ BEGIN
  tab(i) := SUBSTR(list, index_start);
  tablen := i;
 END;
+
+CREATE OR REPLACE PROCEDURE COMMA_TO_TABLE (
+ list IN VARCHAR2,
+ tablen OUT BINARY_INTEGER,
+ tab OUT DBMS_UTILITY.lname_array)
+IS
+ i BINARY_INTEGER := 1;
+ index_start PLS_INTEGER := 1;
+BEGIN
+ WHILE INSTR(list, ',', 1, i) != 0 LOOP
+  tab(i) := SUBSTR(list, index_start, INSTR(list, ',', 1, i) - index_start);
+  index_start := INSTR(list, ',', 1, i) + 1;
+  i := i + 1; 
+  END LOOP;
+ tab(i) := SUBSTR(list, index_start);
+ tablen := i;
+END;
